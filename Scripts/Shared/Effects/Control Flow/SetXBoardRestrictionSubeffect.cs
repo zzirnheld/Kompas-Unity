bebeac67d644c93d937aa68a498ec6d0d0e437c6
Eimti <eimtixa@gmail.com>
2019-09-03 10:05:31 -0400

    Set X by Board Restriction count subeffect

---
+++ Scripts/Shared/Effects/Control Flow/SetXBoardRestrictionSubeffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetXBoardRestrictionSubeffect : Subeffect
+{
+    public BoardRestriction boardRestriction;
+
+    public override void Resolve()
+    {
+        parent.X = 0;
+        for(int i = 0; i < 7; i++)
+        {
+            for(int j = 0; j < 7; j++)
+            {
+                Card c = parent.serverGame.boardCtrl.GetCardAt(i, j);
+                if (boardRestriction.Evaluate(c)) parent.X++;
+                foreach(Card aug in c.Augments)
+                {
+                    if (boardRestriction.Evaluate(aug)) parent.X++;
+                }
+            }
+        }
+        parent.ResolveNextSubeffect();
+    }
+}
