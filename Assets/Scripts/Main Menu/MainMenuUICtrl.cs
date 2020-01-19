73841a936b9c06d680c365e672d86a71ecb10d49
Eimti <31927511+Eimti@users.noreply.github.com>
2020-01-19 14:03:27 -0500

    Main Menu Beginnings
    Preserves existing functionality behind an additional button, so I'm keeping it on master.

---
+++ Assets/Scripts/Main Menu/MainMenuUICtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUICtrl : MonoBehaviour
+{
+    public const int ClientScene = 1;
+    public const int ServerScene = 2;
+    public const int DeckbuildScene = 3;
+
+    public void StartServer()
+    {
+        //load the server scene
+        SceneManager.LoadScene(ServerScene);
+    }
+
+    public void StartClient()
+    {
+        //load the client scene
+        SceneManager.LoadScene(ClientScene);
+    }
+
+    public void BuildDeck()
+    {
+        //load the deck building scene
+        SceneManager.LoadScene(DeckbuildScene);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
