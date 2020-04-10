07490202a1369ecc113847f06511a55b285c175c
Eimti <eimtixa@gmail.com>
2020-04-10 18:54:22 -0400

    Add ListRestriction

---
+++ Assets/Scripts/Shared/Effects/Dummy Targeting/DummyListTargetSubeffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyListTargetSubeffect : Subeffect
+{
+    /// <summary>
+    /// Restriction that each card must fulfill
+    /// </summary>
+    public CardRestriction CardRestriction;
+
+    /// <summary>
+    /// Restriction that the list collectively must fulfill
+    /// </summary>
+    public ListRestriction ListRestriction;
+
+    public override void Resolve()
+    {
+        throw new System.NotImplementedException(("Dummy List Target Subeffect should never resolve."));
+    }
+
+    public override void Initialize()
+    {
+        CardRestriction.Subeffect = this;
+        ListRestriction.Subeffect = this;
+    }
+}
---
+++ Assets/Scripts/Shared/Effects/Restrictions/ListRestriction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListRestriction
+{
+    public Subeffect Subeffect;
+
+    public enum ListRestrictions
+    {
+        CanPayCost = 0 //effect's controller is able to pay the cost of all of them together
+    }
+    public ListRestrictions[] Restrictions;
+
+    /// <summary>
+    /// Checks the list of cards passed into see if they collectively fit a restriction.
+    /// </summary>
+    /// <param name="cards">The list of cards to collectively evaluate.</param>
+    /// <returns><see langword="true"/> if the cards fit all the required restrictions collectively, 
+    /// <see langword="false"/> otherwise</returns>
+    public bool Evaluate(IEnumerable<Card> cards)
+    {
+        foreach(var restriction in Restrictions)
+        {
+            switch (restriction)
+            {
+                case ListRestrictions.CanPayCost:
+                    int totalCost = 0;
+                    foreach(var card in cards)
+                    {
+                        //TODO: check each card's playability, once runes are in?
+                        totalCost += card.Cost;
+                    }
+                    if (Subeffect.EffectController.pips < totalCost) return false;
+                    break;
+                default:
+                    Debug.LogError($"Could not check list restriction {restriction}");
+                    return false;
+            }
+        }
+        return true;
+    }
+}
