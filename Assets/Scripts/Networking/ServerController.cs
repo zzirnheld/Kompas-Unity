c9a20b595d309439b0f02e2f99a3be4d27b58a7b
Eimti <eimtixa@gmail.com>
2020-06-14 16:33:45 -0400

    Don't block on awaiting connection

--- Assets/Scripts/Networking/ServerController.cs
+++ Assets/Scripts/Networking/ServerController.cs
@@ -47,7 +47,7 @@ namespace KompasNetworking
                     games.Add(currGame);
                 }
                 Debug.Log("Waiting for next client");
-                var client = listener.AcceptTcpClient();
+                var client = await listener.AcceptTcpClient();
                 Debug.Log("Connected to a client");
                 if(currGame.AddPlayer(client) >= 2) currGame = null;
             }
