9f5048157f3c50612c7956436ae2557ec34e9fee
Eimti <eimtixa@gmail.com>
2020-07-13 22:25:15 -0400

    Deck target subeffect shuffle before next subeff

--- Assets/Scripts/Server/Effects/Targeting/DeckTargetSubeffect.cs
+++ Assets/Scripts/Server/Effects/Targeting/DeckTargetSubeffect.cs
@@ -24,9 +24,14 @@ public class DeckTargetSubeffect : CardTargetSubeffect
 
     public override bool AddTargetIfLegal(GameCard card)
     {
-        if (base.AddTargetIfLegal(card))
+        //evaluate the target. if it's valid, confirm it as the target (that's what the true is for)
+        if (cardRestriction.Evaluate(card))
         {
+            Debug.Log("Adding " + card.CardName + " as target");
+            ServerEffect.AddTarget(card);
+            EffectController.ServerNotifier.AcceptTarget();
             card.Controller.deckCtrl.Shuffle();
+            ServerEffect.ResolveNextSubeffect();
             return true;
         }
 
