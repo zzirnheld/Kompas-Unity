fafd62cd6e9e438d94e470e75382bf04f50f6a3c
Eimti <31927511+Eimti@users.noreply.github.com>
2020-08-11 15:41:59 -0400

    Resolve compiler warnings (hiding Player in subeffect)

--- Assets/Scripts/Server/Effects/Card Movement Between States/DrawSubeffect.cs
+++ Assets/Scripts/Server/Effects/Card Movement Between States/DrawSubeffect.cs
@@ -2,12 +2,9 @@
 {
     public class DrawSubeffect : ServerSubeffect
     {
-        public int Player = -1;
-        public int WhoDraws { get { return Player == -1 ? ServerEffect.ServerController.index : Player; } }
-
         public override bool Resolve()
         {
-            var drawn = ServerGame.Draw(WhoDraws);
+            var drawn = ServerGame.Draw(Player.index);
             if (drawn == null) return ServerEffect.EffectImpossible();
             else return ServerEffect.ResolveNextSubeffect();
         }
--- Assets/Scripts/Server/Effects/Stats/AddPipsSubeffect.cs
+++ Assets/Scripts/Server/Effects/Stats/AddPipsSubeffect.cs
@@ -5,10 +5,6 @@
         public int xMultiplier = 0;
         public int xDivisor = 1;
         public int modifier = 0;
-        public int playerOffset = 0; //0 for controller, 1 for controller's enemy
-
-        //could also be                                         playerOffest == parent.effectController ? 0 : 1
-        private Player Player => ServerEffect.serverGame.Players[playerOffset + ServerEffect.ServerController.index % 2];
         private int Count => (xMultiplier * ServerEffect.X / xDivisor) + modifier;
 
         public override bool Resolve()
