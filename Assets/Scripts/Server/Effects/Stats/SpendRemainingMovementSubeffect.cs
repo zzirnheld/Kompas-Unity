28f7a7de2defad684e5ba0044e1941cea4165546
Eimti <eimtixa@gmail.com>
2020-12-03 16:23:32 -0500

    Update linewalkers to spend all remaining movement

---
+++ Assets/Scripts/Server/Effects/Stats/SpendRemainingMovementSubeffect.cs
@@ -0,0 +1,17 @@
+﻿namespace KompasServer.Effects
+{
+    public class SpendRemainingMovementSubeffect : ServerSubeffect
+    {
+        public int mult = 1;
+        public int div = 1;
+        public int mod = 0;
+
+        public override bool Resolve()
+        {
+            int toSpend = (Target.SpacesCanMove * mult / div) + mod;
+            int toSet = Target.SpacesMoved + toSpend;
+            Target.SetSpacesMoved(toSet);
+            return ServerEffect.ResolveNextSubeffect();
+        }
+    }
+}
