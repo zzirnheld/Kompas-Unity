7473d7a8f11baf7e787f9403e3c7f3cfcd3c7bef
Eimti <eimtixa@gmail.com>
2020-12-21 12:53:38 -0500

    Make save the people shuffle deck first

---
+++ Assets/Scripts/Server/Effects/Card Movement Between States/ShuffleDeckSubeffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KompasServer.Effects
+{
+    public class ShuffleDeckSubeffect : ServerSubeffect
+    {
+        public override bool Resolve()
+        {
+            Player.deckCtrl.Shuffle();
+            return ServerEffect.ResolveNextSubeffect();
+        }
+    }
+}
