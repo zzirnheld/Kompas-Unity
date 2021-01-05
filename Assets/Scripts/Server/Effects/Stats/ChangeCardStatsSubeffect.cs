cd83dcb2632bbe7c9593c1458d7592d7b2e3d78d
Eimti <eimtixa@gmail.com>
2021-01-04 22:15:07 -0500

    Fix Dagaz

--- Assets/Scripts/Server/Effects/Stats/ChangeCardStatsSubeffect.cs
+++ Assets/Scripts/Server/Effects/Stats/ChangeCardStatsSubeffect.cs
@@ -10,6 +10,13 @@
         public int cMult = 0;
         public int aMult = 0;
 
+        public int nDiv = 1;
+        public int eDiv = 1;
+        public int sDiv = 1;
+        public int wDiv = 1;
+        public int cDiv = 1;
+        public int aDiv = 1;
+
         public int nMod = 0;
         public int eMod = 0;
         public int sMod = 0;
@@ -17,12 +24,12 @@
         public int cMod = 0;
         public int aMod = 0;
 
-        public int NVal => ServerEffect.X * nMult + nMod;
-        public int EVal => ServerEffect.X * eMult + eMod;
-        public int SVal => ServerEffect.X * sMult + sMod;
-        public int WVal => ServerEffect.X * wMult + wMod;
-        public int CVal => ServerEffect.X * cMult + cMod;
-        public int AVal => ServerEffect.X * aMult + aMod;
+        public int NVal => ServerEffect.X * nMult / nDiv  + nMod;
+        public int EVal => ServerEffect.X * eMult / eDiv + eMod;
+        public int SVal => ServerEffect.X * sMult / sDiv + sMod;
+        public int WVal => ServerEffect.X * wMult / wDiv + wMod;
+        public int CVal => ServerEffect.X * cMult / cDiv + cMod;
+        public int AVal => ServerEffect.X * aMult / aDiv + aMod;
         public (int, int, int, int, int, int) StatValues => (NVal, EVal, SVal, WVal, CVal, AVal);
 
         public override bool Resolve()
