eabd7d53d0b139b6782ebbc1d2b1e08b82e15327
Eimti <eimtixa@gmail.com>
2021-01-07 14:53:32 -0500

    Make ServerEffect work with async

--- Assets/Scripts/Server/Effects/ServerSubeffect.cs
+++ Assets/Scripts/Server/Effects/ServerSubeffect.cs
@@ -2,6 +2,7 @@
 using KompasCore.Effects;
 using KompasCore.GameCore;
 using KompasServer.GameCore;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace KompasServer.Effects
@@ -21,20 +22,6 @@ namespace KompasServer.Effects
         public ServerPlayer ServerPlayer => Player as ServerPlayer;
 
         /// <summary>
-        /// Server Subeffect resolve method. Does whatever this type of subeffect does,
-        /// then executes the next subeffect.<br></br>
-        /// Usually, this involves a call to ResolveNextSubeffect(),
-        /// but if the subeffect does control flow, it calles ResolveSubeffect for a specific thing.<br></br>
-        /// TODO refactor to return an enum/int to represent different control flow states (negatives being special values):<br></br>
-        ///     effect impossible, 
-        ///     resolve next, 
-        ///     resolve specific, 
-        ///     awaiting player input.<br></br>
-        /// <returns><see langword="true"/> if the effect finished resolving successfully, <see langword="false"/> if it's awaiting response</returns>
-        /// </summary>
-        public abstract bool Resolve();
-
-        /// <summary>
         /// Sets up the subeffect with whatever necessary values.
         /// Usually also initializes any restrictions the effects are using.
         /// </summary>
@@ -48,14 +35,43 @@ namespace KompasServer.Effects
         }
 
         /// <summary>
+        /// Server Subeffect resolve method. Does whatever this type of subeffect does,
+        /// then returns a ResolutionInfo struct containing what to do next.
+        /// <returns><see langword="true"/> if the effect finished resolving successfully, <see langword="false"/> if it's awaiting response</returns>
+        /// </summary>
+        public virtual async Task<ResolutionInfo> Resolve() { return ResolutionInfo.Next; }
+
+
+        /// <summary>
         /// Optional method. If implemented, does something when the effect is declared impossible.
         /// Default implementation just finishes resolution of the effect
         /// </summary>
-        public virtual bool OnImpossible()
+        public virtual async Task<ResolutionInfo> OnImpossible(string why)
         {
             Debug.Log($"Base On Impossible called for {GetType()}");
             ServerEffect.OnImpossible = null;
-            return ServerEffect.EffectImpossible();
+            return ResolutionInfo.Impossible(why);
         }
+    }
+
+    public struct ResolutionInfo
+    {
+        public const string EndedBecauseImpossible = "Ended because effect was impossible";
+
+        public ResolutionResult result;
+
+        public int index;
+
+        public string reason;
+
+        public static ResolutionInfo Next => new ResolutionInfo { result = ResolutionResult.Next };
+        public static ResolutionInfo Index(int index) => new ResolutionInfo { result = ResolutionResult.Index, index = index };
+        public static ResolutionInfo Impossible(string why) => new ResolutionInfo { result = ResolutionResult.Impossible, reason = why };
+        public static ResolutionInfo End(string why) => new ResolutionInfo { result = ResolutionResult.End, reason = why };
+    }
+
+    public enum ResolutionResult
+    {
+        Next, Index, Impossible, End
     }
 }
