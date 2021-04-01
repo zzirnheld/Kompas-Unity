67d37ec82bf9f03dcd2b0fba9479da63cff794a8
Eimti <eimtixa@gmail.com>
2021-04-01 18:38:37 -0400

    Update Bound and Trapped offerings

---
+++ Assets/Scripts/Server/Effects/Control Flow/SetXByTargetCardValueSubeffect.cs
@@ -0,0 +1,12 @@
+﻿using KompasCore.Effects;
+using System.Threading.Tasks;
+
+namespace KompasServer.Effects
+{
+    public class SetXByTargetCardValueSubeffect : SetXSubeffect
+    {
+        public CardValue cardValue;
+
+        public override int BaseCount => cardValue.GetValueOf(Target);
+    }
+}
---
+++ Assets/Scripts/Server/Effects/Control Flow/SetXByTriggeringCardValue.cs
@@ -0,0 +1,12 @@
+﻿using KompasCore.Effects;
+using System.Threading.Tasks;
+
+namespace KompasServer.Effects
+{
+    public class SetXByTriggeringCardValueSubeffect : SetXSubeffect
+    {
+        public CardValue cardValue;
+
+        public override int BaseCount => cardValue.GetValueOf(Effect.CurrActivationContext.CardInfo);
+    }
+}
