8583d3e284acf059cb75a470c88f9f24e260d28f
Eimti <eimtixa@gmail.com>
2021-05-19 13:55:08 -0400

    Show "saved!" popup when you click save

---
+++ Assets/Scripts/Deck Build/SavedPopupController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavedPopupController : MonoBehaviour
+{
+    public const float SavedPopupShowDuration = 1;
+
+    private float showTime;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        showTime = 0f;
+    }
+
+    private void Update()
+    {
+        showTime += Time.deltaTime;
+        if (showTime > SavedPopupShowDuration) gameObject.SetActive(false);
+    }
+}
