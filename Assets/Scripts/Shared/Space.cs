1828171873994445852aa23d57b3aec53604a283
Eimti <eimtixa@gmail.com>
2021-06-01 18:04:02 -0400

    Update GameCard to use Space struct

---
+++ Assets/Scripts/Shared/Space.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Space
+{
+    public int x;
+    public int y;
+
+    public Space(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public bool Valid => x >= 0 && y >= 0 && x < 7 && y < 7;
+    public bool Corner => (x == 0 || x == 6) && (y == 0 || y == 6);
+
+    public int Index => 7 * x + y;
+    public Space Inverse => new Space(6 - x, 6 - y);
+
+    public int DistanceTo(Space other) => Math.Max(Math.Abs(x - other.x), Math.Abs(y - other.y));
+
+    public bool AdjacentTo(Space other) => DistanceTo(other) == 1;
+
+    public bool SameColumn(Space other) => x - y == other.x - other.y;
+    public bool SameDiagonal(Space other) => x == other.x || y == other.y;
+
+    public bool NorthOf(Space other) => x > other.x || y > other.y;
+    public Space DueNorth => new Space(x + 1, y + 1);
+
+
+    public override string ToString() => $"{x}, {y}";
+
+    public static bool operator ==(Space a, Space b) => a.x == b.x && a.y == b.y;
+    public static bool operator !=(Space a, Space b) => !(a == b);
+}
