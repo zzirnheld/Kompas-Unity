f20cb04992bf0f3925bef74ac846ebb2348ce132
Eimti <eimtixa@gmail.com>
2021-08-19 22:09:38 -0400

    Various initial sd bugfixes

--- Assets/Scripts/Shared/Effects/Restrictions/TriggerRestriction.cs
+++ Assets/Scripts/Shared/Effects/Restrictions/TriggerRestriction.cs
@@ -129,7 +129,7 @@ namespace KompasCore.Effects
                 case TriggererNowFitsRestirction: return nowRestriction.Evaluate(context.CardInfo.Card);
                 case TriggerersAugmentedCardFitsRestriction: return cardRestriction.Evaluate(context.CardInfo.AugmentedCard);
                 case StackableSourceFitsRestriction: return sourceRestriction.Evaluate(context.Stackable?.Source);
-                case TriggererIsSecondaryContextTarget: return secondary?.Targets.Any(c => c == context.CardInfo?.Card) ?? false;
+                case TriggererIsSecondaryContextTarget: return secondary?.Targets?.Any(c => c == context.CardInfo?.Card) ?? false;
 
                 case CardNowFurtherFromSourceThanItWas:
                     return ThisCard.DistanceTo(context.CardInfo.Card.Position) > ThisCard.DistanceTo(context.CardInfo.Position); 
