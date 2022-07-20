b96804a1ab46bbd14f925c0e5e72d228e9774b81
Eimti <eimtixa@gmail.com>
2022-07-20 09:47:52 -0500

    New PLATOON

--- Assets/Scripts/Shared/Effects/Restrictions/Card Restriction Elements/CardRestrictionElement.cs
+++ Assets/Scripts/Shared/Effects/Restrictions/Card Restriction Elements/CardRestrictionElement.cs
@@ -144,19 +144,37 @@ namespace KompasCore.Effects.Restrictions
             /// Can be null to represent checking whether the card is in any fight at all
             /// </summary>
             public IActivationContextIdentity<GameCardBase> fightingWho;
+            public bool defending = false;
+            public bool attacking = false;
 
             public override void Initialize(EffectInitializationContext initializationContext)
             {
                 base.Initialize(initializationContext);
                 fightingWho?.Initialize(initializationContext);
+
+                if (attacking && defending)
+                    throw new System.ArgumentException("Can't require a card to be attacking and defending using the same Fighting object");
             }
 
             private bool IsValidFight(GameCardBase card, ActivationContext context, IStackable stackEntry)
             {
+                if (!(stackEntry is Attack attack)) return false;
+
+                //If need to be attacking or defending, check those
+                if (attacking)
+                {
+                    if (attack.attacker != card) return false;
+                }
+                else if (defending)
+                {
+                    if (attack.defender != card) return false;
+                }
+                //Otherwise, just make sure the character is in the fight at all
+                else if (attack.attacker != card && attack.defender != card) return false;
+
+                //And if that card is in the fight, make sure any other card that needs to be in the fight, is in the fight.
                 var fightingWhoCard = fightingWho?.From(context, default);
-                return stackEntry is Attack attack
-                    && (attack.attacker == card || attack.defender == card)
-                    && (fightingWho == null || attack.attacker == fightingWhoCard || attack.defender == fightingWhoCard);
+                return fightingWho == null || attack.attacker == fightingWhoCard || attack.defender == fightingWhoCard;
             }
 
             protected override bool FitsRestrictionLogic(GameCardBase card, ActivationContext context)
