421a1e76a47c6bde2aa207b8d672e070d557cc53
Eimti <eimtixa@gmail.com>
2022-09-19 11:03:09 -0500

    Prepare StacableEntitiesController for extension

--- Assets/Scripts/Shared/UI/StackableEntitiesController.cs
+++ Assets/Scripts/Shared/UI/StackableEntitiesController.cs
@@ -5,22 +5,26 @@ namespace KompasCore.UI
 {
     public class StackableEntitiesController : MonoBehaviour
     {
-        [Header("Item that is going to be stacked")]
+        [Header("Offsets")]
+        [Tooltip("Between objects while collapsed")]
         public Vector3 collapsedOffset;
+        [Tooltip("Between objects while expanded")]
         public Vector3 expandedOffset;
-
-        [Tooltip("Offset for the entire stack while it's expanded")]
+        [Tooltip("Offset for the entire from parent while it's expanded")]
         public float whileExpandedOffset;
 
+        [Header("Collider attributes")]
         public int layer;
         public float colliderPadding = 1.1f;
         public BoxCollider boxCollider;
 
-        public int LayerMask => 1 << layer;
-
         private List<GameObject> objects = null;
 
         private bool collapsed = true;
+        private Vector3 currOffset = Vector3.zero;
+
+        private int LayerMask => 1 << layer;
+        private Vector3 LocalMeToCamera => transform.InverseTransformVector(Camera.main.transform.position - transform.position).normalized;
 
         public void Initalize(ICollection<GameObject> objects)
         {
@@ -30,23 +34,34 @@ namespace KompasCore.UI
             Collapse();
         }
 
-        private Vector3 LocalMeToCamera => transform.InverseTransformVector(Camera.main.transform.position - transform.position).normalized;
-
         public void Collapse()
         {
             if (objects == null) throw new System.ArgumentException("Tried to collapse a StackableEntitiesController that wasn't initialized!");
-            if (!collapsed) transform.localPosition = transform.localPosition + LocalMeToCamera * (-1f * whileExpandedOffset);
-            ShowWithOffset(collapsedOffset);
+            OffsetSelf(0f);
+            ShowCollapsed();
+            UpdateColliders();
             collapsed = true;
         }
 
         public void Expand()
         {
             if (objects == null) throw new System.ArgumentException("Tried to expand a StackableEntitiesController that wasn't initialized!");
-            if (collapsed) transform.localPosition = transform.localPosition + LocalMeToCamera * (whileExpandedOffset);
-            ShowWithOffset(expandedOffset);
+            OffsetSelf(whileExpandedOffset);
+            ShowExpanded();
+            UpdateColliders();
             collapsed = false;
         }
+
+        private void OffsetSelf(float offset)
+        {
+            transform.localPosition -= currOffset;
+            currOffset = LocalMeToCamera * offset;
+            transform.localPosition += currOffset;
+        }
+
+        protected virtual void ShowCollapsed() => ShowWithOffset(collapsedOffset);
+
+        protected virtual void ShowExpanded() => ShowWithOffset(expandedOffset);
 
         private void ShowWithOffset(Vector3 offset)
         {
@@ -54,9 +69,9 @@ namespace KompasCore.UI
             {
                 obj.transform.localPosition = index * offset;
             }
-            UpdateColliders();
         }
 
+        #region collider
         private Bounds GetChildRendererBounds(GameObject go)
         {
             var renderers = go.GetComponentsInChildren<Renderer>();
@@ -90,16 +105,14 @@ namespace KompasCore.UI
             boxCollider.size = totalBounds.size * colliderPadding;
             boxCollider.center = transform.InverseTransformPoint(totalBounds.center) + (0.5f * Vector3.down);
         }
+        #endregion collider
 
         private void Update()
         {
             bool success = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, Mathf.Infinity, LayerMask)
                 && hit.collider == boxCollider;
-            if (success && collapsed)
-            {
-                Debug.Log("EXPANDING");
-                Expand();
-            }
+
+            if (success && collapsed) Expand();
             else if (!success && !collapsed) Collapse();
         }
     }
