06eb7bab3fa00f459e16a0227a4c963aeb163a5d
Eimti <eimtixa@gmail.com>
2022-10-30 14:57:52 -0500

    Add guild form reviewer, fix not being able to hover over augments

--- Assets/Scripts/Shared/HandController.cs
+++ Assets/Scripts/Shared/HandController.cs
@@ -52,7 +52,7 @@ namespace KompasCore.GameCore
             for (int i = 0; i < hand.Count; i++)
             {
                 hand[i].CardController.transform.parent = transform;
-                hand[i].CardController.transform.localPosition = new Vector3((-0.8f * (float)hand.Count) + ((float)i * 2f), 0, 0);
+                hand[i].CardController.transform.localPosition = new Vector3((-0.9f * (float)hand.Count) + ((float)i * 2.25f), 0, 0);
                 hand[i].CardController.SetRotation();
                 hand[i].CardController.gameObject.SetActive(true);
             }
