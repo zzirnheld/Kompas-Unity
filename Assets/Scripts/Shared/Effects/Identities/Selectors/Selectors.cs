76738cefae07eff3de8a1348cdb644f07ebe9ab3
zzirnheld <zzirnheld@gmail.com>
2022-12-21 09:26:46 -0600

    Add Random selector, basic Masquerade metas

--- Assets/Scripts/Shared/Effects/Identities/Selectors/Selectors.cs
+++ Assets/Scripts/Shared/Effects/Identities/Selectors/Selectors.cs
@@ -1,8 +1,17 @@
+using System.Linq;
+using System.Collections.Generic;
 
 namespace KompasCore.Effects.Selectors
 {
     public interface Selector
     {
-        
+        T Select<T>(IReadOnlyCollection<T> objects);
+    }
+
+    public class RandomSelector : Selector
+    {
+        System.Random random = new System.Random();
+
+        public T Select<T>(IReadOnlyCollection<T> objects) => objects.ElementAt(random.Next(0, objects.Count));
     }
 }
