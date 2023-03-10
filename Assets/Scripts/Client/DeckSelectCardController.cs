8105c4e6883d9f22029a135455eaf22ed8d06ea8
zzirnheld <zzirnheld@gmail.com>
2023-03-10 16:56:06 -0600

    Can now select a deck and see the cards in it in the new deck select UI

--- Assets/Scripts/Client/DeckSelectCardController.cs
+++ Assets/Scripts/Client/DeckSelectCardController.cs
@@ -7,7 +7,34 @@ namespace KompasClient.UI
 {
     public class DeckSelectCardController : ImageOnlyCardViewController, IPointerDownHandler
     {
-        public DeckSelectCard Card { get; private set; }
+        private class DeckSelectCard : CardBase
+        {
+            public override int BaseN { get; }
+            public override int BaseE { get; }
+            public override int BaseS { get; }
+            public override int BaseW { get; }
+            public override int BaseC { get; }
+            public override int BaseA { get; }
+
+            public DeckSelectCard(SerializableCard card, string fileName)
+                : base((card.n, card.e, card.s, card.w, card.c, card.a),
+                        card.subtext, card.spellTypes,
+                        card.unique,
+                        card.radius, card.duration,
+                        card.cardType, card.cardName, fileName,
+                        card.effText,
+                        card.subtypeText)
+            {
+                BaseN = card.n;
+                BaseE = card.e;
+                BaseS = card.s;
+                BaseW = card.w;
+                BaseC = card.c;
+                BaseA = card.a;
+            }
+        }
+        
+        public CardBase Card { get; private set; }
         private DeckSelectUIController uiController;
         public void SetInfo(SerializableCard card, DeckSelectUIController uiCtrl, string fileName)
         {
@@ -19,33 +46,6 @@ namespace KompasClient.UI
         public void OnPointerDown(PointerEventData eventData)
         {
             uiController.SelectAsAvatar(this);
-        }
-    }
-
-    public class DeckSelectCard : CardBase
-    {
-        public override int BaseN { get; }
-        public override int BaseE { get; }
-        public override int BaseS { get; }
-        public override int BaseW { get; }
-        public override int BaseC { get; }
-        public override int BaseA { get; }
-
-        public DeckSelectCard(SerializableCard card, string fileName)
-            : base((card.n, card.e, card.s, card.w, card.c, card.a),
-                       card.subtext, card.spellTypes,
-                       card.unique,
-                       card.radius, card.duration,
-                       card.cardType, card.cardName, fileName,
-                       card.effText,
-                       card.subtypeText)
-        {
-            BaseN = card.n;
-            BaseE = card.e;
-            BaseS = card.s;
-            BaseW = card.w;
-            BaseC = card.c;
-            BaseA = card.a;
         }
     }
 }
--- Assets/Scripts/Client/UI/Game Start/NewDeckSelectUIController.cs
+++ Assets/Scripts/Client/UI/Game Start/NewDeckSelectUIController.cs
@@ -1,14 +1,68 @@
+using System.Collections.Generic;
+using KompasClient.Cards;
+using KompasCore.Helpers;
 using KompasCore.UI;
+using TMPro;
+using UnityEngine;
 
 namespace KompasClient.UI
 {
     public class NewDeckSelectUIController : DeckDropdownControllerBase
     {
+        public ClientCardRepository cardRepository;
+        public DeckSelectCardController deckSelectCardPrefab;
+        public Transform deckScrollAreaParentTransform;
+        public TMP_Text cardsInDeckText;
+
+        private IList<DeckSelectCardController> currDeck = new List<DeckSelectCardController>();
+
         private void Start() => Load();
+
+        public void SelectionChanged(int index) => Show(dropdown.options[index].text);
 
         protected override void Show(string deckName)
         {
-            //throw new System.NotImplementedException();
+            var cardNames = DeckLoadHelper.LoadDeck(deckName); //TODO stash, using the override of LoadDeck in DeckDropdownControllerBase
+
+            ClearDeck();
+
+            foreach (string name in cardNames)
+            {
+                if (!string.IsNullOrWhiteSpace(name)) AddToDeck(name);
+            }
+
+            SetDeckCountText();
+        }
+
+        private void SetDeckCountText()
+        {
+            cardsInDeckText.text = $"Cards in Deck: {currDeck.Count}";
+        }
+
+        private void ClearDeck()
+        {
+            foreach (var card in currDeck) Destroy(card.gameObject);
+            currDeck.Clear();
+        }
+
+        private void AddToDeck(string name)
+        {
+            string json = cardRepository.GetJsonFromName(name);
+            if (json == null)
+            {
+                Debug.LogError($"No json found for card name {name}");
+                return;
+            }
+
+            DeckSelectCardController toAdd = cardRepository.InstantiateDeckSelectCard(json, deckScrollAreaParentTransform, deckSelectCardPrefab, null);
+            if (toAdd == null)
+            {
+                Debug.LogError($"Somehow have a DeckbuilderCard with name {name} couldn't be re-instantiated");
+                return;
+            }
+
+            currDeck.Add(toAdd);
+            SetDeckCountText();
         }
     }
 }
