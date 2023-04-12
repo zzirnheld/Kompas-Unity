c75efb076d99e825eebbe69a7b47ed5468fc4687
zzirnheld <zzirnheld@gmail.com>
2023-04-12 14:54:20 -0500

    "NOt Augment" made me realize I needed a can move restriction

--- Assets/Scripts/Shared/Effects/Restrictions/Card Restriction Elements/CanMove.cs
+++ Assets/Scripts/Shared/Effects/Restrictions/Card Restriction Elements/CanMove.cs
@@ -1,19 +1,42 @@
+using System.Linq;
 using KompasCore.Cards;
 using KompasCore.Effects.Identities;
+using KompasServer.Effects.Subeffects;
 
 namespace KompasCore.Effects.Restrictions.CardRestrictionElements
 {
     public class CanMove : CardRestrictionElement
     {
         public IIdentity<Space> destination;
+        public int spaceRestrictionSubeffectIndex;
 
         public override void Initialize(EffectInitializationContext initializationContext)
         {
             base.Initialize(initializationContext);
             destination?.Initialize(initializationContext);
+            if (spaceRestrictionSubeffectIndex != default
+                && InitializationContext.effect.Subeffects[spaceRestrictionSubeffectIndex] is not SpaceTarget)
+            {
+                throw new System.ArgumentException($"{spaceRestrictionSubeffectIndex} isn't a space target subeffect! for {InitializationContext.effect}");
+            }
         }
 
         protected override bool IsValidLogic(GameCardBase card, IResolutionContext context)
-            => card.MovementRestriction.IsValidEffectMove(destination.From(context, default), context);
+        {
+            bool IsValidMoveSpace(Space space) => card.MovementRestriction.IsValidEffectMove(space, context);
+
+            if (destination != null) return IsValidMoveSpace(destination.From(context, default));
+            else if (spaceRestrictionSubeffectIndex != default)
+            {
+                if (InitializationContext.effect.Subeffects[spaceRestrictionSubeffectIndex] is not SpaceTarget spaceTargetSubeffect)
+                {
+                    throw new System.ArgumentException($"{spaceRestrictionSubeffectIndex} isn't a space target subeffect! resolving {InitializationContext.effect}");
+                }
+
+                bool IsSubeffectRestrictionValid(Space space) => spaceTargetSubeffect.spaceRestriction.IsValidSpace(space, context);
+                return Space.Spaces.Where(IsSubeffectRestrictionValid).Any(IsValidMoveSpace);
+            }
+            else return Space.Spaces.Any(IsValidMoveSpace);
+        }
     }
 }
