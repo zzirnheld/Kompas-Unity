3465d82db7a8a99ffae8cb1d0e608984786aab5c
zzirnheld <zzirnheld@gmail.com>
2023-04-23 14:39:32 -0500

    Make the hand camera controller default to 2f via script

--- Assets/Scripts/Client/HandCameraController.cs
+++ Assets/Scripts/Client/HandCameraController.cs
@@ -15,6 +15,8 @@ namespace KompasClient.UI
 			var renderTexture = new RenderTexture((int)textureRectTransform.rect.width * 2, (int)textureRectTransform.rect.height * 2, 24);
 			gridCamera.targetTexture = renderTexture;
 			rawImage.texture = renderTexture;
-		}
+
+            gridCamera.gameObject.transform.localPosition = 2f * Vector3.up;
+        }
 	}
 }
