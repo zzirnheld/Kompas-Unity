4f3027539d7f1ff30af5013e342b5357ece2aac9
zzirnheld <zzirnheld@gmail.com>
2023-04-26 17:14:32 -0500

    Make everything use the default restrictions for activation, if at all possible

--- Assets/Scripts/Shared/Effects/Restrictions/ActivationRestriction.cs
+++ Assets/Scripts/Shared/Effects/Restrictions/ActivationRestriction.cs
@@ -1,44 +1,7 @@
-﻿using KompasCore.Cards;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-
-namespace KompasCore.Effects
+﻿namespace KompasCore.Effects
 {
 	public class ActivationRestriction : Restrictions.PlayerRestrictionElements.AllOf
 	{
-		public Effect Effect => InitializationContext.effect;
-		public GameCard Card => InitializationContext.source;
-
-		public const string Never = "Never";
-
-		public const string Default = "Default";
-
-		public int maxTimes = 1;
-		private readonly List<string> ActivationRestrictions = new List<string>();
-		public string[] activationRestrictionArray = null;
-
-		public override void Initialize(EffectInitializationContext initializationContext)
-		{
-			base.Initialize(initializationContext);
-
-			if (activationRestrictionArray == null) ActivationRestrictions.Add(Never);
-			else
-			{
-				ActivationRestrictions.AddRange(activationRestrictionArray);
-
-				Debug.Log($"Initializing activation restriction for {Card.CardName} " +
-					$"with restrictions: {string.Join(", ", ActivationRestrictions)}");
-			}
-		}
-
-		private bool IsRestrictionValid(string r, Player activator) => r switch
-		{
-			Never => false,
-			Default => true,
-
-			_ => throw new System.ArgumentException($"Invalid activation restriction {r}")
-		};
 
 		/* This exists to debug a card's activation restriction,
 		 * but should not be usually used because it prints a ton whenever
@@ -50,16 +13,6 @@ namespace KompasCore.Effects
 				$"flouts restriction {restriction} for activator {activator.index}");
 			return valid;
 		} */
-
-		private bool IsGameSetUp() => Card != null && Card.Game != null;
-
-		public bool IsValidActivation(Player activator)
-			=> IsGameSetUp() && ActivationRestrictions.All(r => IsRestrictionValid(r, activator));
-
-		protected override bool IsValidLogic(Player item, IResolutionContext context)
-		{
-			return base.IsValidLogic(item, context) && IsValidActivation(item);
-		}
 
         public bool IsPotentiallyValidActivation(Player activator) => throw new System.NotImplementedException(); //TODO with strings to type names?
     }
