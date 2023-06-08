6f78e0688ddf329d2e097b2dee618c2cd689f632
zzirnheld <zzirnheld@gmail.com>
2023-06-08 14:34:34 -0500

    Handle activation in the discard

--- Assets/Scripts/Shared/Effects/Restrictions/Player Restriction Elements/PlayerRestrictionElement.cs
+++ Assets/Scripts/Shared/Effects/Restrictions/Player Restriction Elements/PlayerRestrictionElement.cs
@@ -26,32 +26,5 @@ namespace KompasCore.Effects.Restrictions
 			protected override bool IsValidLogic(Player item, IResolutionContext context)
 				=> !negated.IsValid(item, context);
 		}
-
-		public class ActivationRestriction : AllOf
-		{
-			public string[] locations = { CardLocation.Board.StringVersion() };
-
-			protected override IEnumerable<IRestriction<Player>> DefaultElements
-			{
-				get
-				{
-					yield return new GamestateRestrictionElements.FriendlyTurn();
-					yield return new TriggerRestrictionElements.CardFitsRestriction()
-					{
-						card = new Identities.Cards.ThisCardNow(),
-						cardRestriction = new CardRestrictionElements.AllOf()
-						{
-							elements = new IRestriction<GameCardBase>[] {
-								new CardRestrictionElements.Location() { locations = this.locations },
-								new CardRestrictionElements.Not() { negated = new CardRestrictionElements.Negated() }
-							}
-						}
-					};
-					yield return new GamestateRestrictionElements.NothingHappening();
-					yield return new GamestateRestrictionElements.Not() { negated = new GamestateRestrictionElements.EffectAlreadyTriggered() };
-					yield return new PlayersMatch() { player = new Identities.Players.FriendlyPlayer() };
-				}
-			}
-		}
 	}
 }
