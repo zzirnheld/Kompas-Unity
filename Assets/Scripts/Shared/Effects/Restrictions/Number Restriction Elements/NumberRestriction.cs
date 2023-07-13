413694a9b2d10e6a31156247b827b509486bb22c
zzirnheld <zzirnheld@gmail.com>
2023-07-13 14:39:30 -0500

    Remove/rework compare with constant

--- Assets/Scripts/Shared/Effects/Restrictions/Number Restriction Elements/NumberRestriction.cs
+++ Assets/Scripts/Shared/Effects/Restrictions/Number Restriction Elements/NumberRestriction.cs
@@ -6,14 +6,6 @@ namespace KompasCore.Effects
 {
 	public class NumberRestriction : AllOfBase<int>
 	{
-		public const string Positive = ">0";
-		public const string Negative = "<0";
-		public const string Nonnegative = ">=0";
-
-		public const string AtLeastConstant = ">=Constant";
-		public const string NoMoreThanConstant = "<=Constant";
-		public const string EqualsConstant = "==Constant";
-
 		public const string LessThanEffectX = "<X";
 		public const string GreaterThanEffectX = ">X";
 		public const string EqualsEffectX = "=X";
@@ -38,14 +30,6 @@ namespace KompasCore.Effects
 
 		private bool IsRestrictionValid(string r, int x) => r switch
 		{
-			Positive => x > 0,
-			Negative => x < 0,
-			Nonnegative => x >= 0,
-
-			AtLeastConstant => x >= constant,
-			NoMoreThanConstant => x <= constant,
-			EqualsConstant => x == constant,
-
 			LessThanEffectX => x < Subeffect.Count,
 			GreaterThanEffectX => x > Subeffect.Count,
 			EqualsEffectX => x == Subeffect.Count,
