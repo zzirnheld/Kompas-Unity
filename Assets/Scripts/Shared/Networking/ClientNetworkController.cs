17bddcff16cc0543be32b650f9353fd38317e97c
zzirnheld <zzirnheld@gmail.com>
2023-09-11 17:01:49 -0500

    Get everything compiling, connected, recolored properly (in the menus) and loading

--- Assets/Scripts/Shared/Networking/ClientNetworkController.cs
+++ Assets/Scripts/Shared/Networking/ClientNetworkController.cs
@@ -1,6 +1,7 @@
 ﻿using KompasClient.GameCore;
 using KompasCore.Networking;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
@@ -51,72 +52,76 @@ namespace KompasClient.Networking
 			//TODO: if !tcpClient.connected
 		}
 
+		private static readonly Dictionary<string, System.Type> jsonTypes = new()
+		{
+			//game start
+			{ Packet.GetDeck, typeof(GetDeckClientPacket)},
+			{ Packet.DeckAccepted, typeof(DeckAcceptedClientPacket)},
+			{ Packet.SetAvatar, typeof(SetAvatarClientPacket)},
+			{ Packet.SetFirstTurnPlayer, typeof(SetFirstPlayerClientPacket)},
+			//game end
+			{ Packet.GameEnd, typeof(GameEndClientPacket)},
+			//gamestate
+			{ Packet.SetLeyload, typeof(SetLeyloadClientPacket)},
+			{ Packet.SetTurnPlayer, typeof(SetTurnPlayerClientPacket)},
+			{ Packet.PutCardsBack, typeof(PutCardsBackClientPacket)},
+			{ Packet.AttackStarted, typeof(AttackStartedClientPacket)},
+			{ Packet.HandSizeToStack, typeof(HandSizeToStackClientPacket)},
+			{ Packet.ChooseHandSize, typeof(GetHandSizeChoicesClientPacket)},
+			{ Packet.SetDeckCount, typeof(SetDeckCountClientPacket)},
+			//card addition/deletion
+			{ Packet.AddCard, typeof(AddCardClientPacket)},
+			{ Packet.DeleteCard, typeof(DeleteCardClientPacket)},
+			{ Packet.ChangeEnemyHandCount, typeof(ChangeEnemyHandCountClientPacket)},
+			//card movement
+			{ Packet.KnownToEnemy, typeof(UpdateKnownToEnemyClientPacket)},
+			//public areas
+			{ Packet.PlayCard, typeof(PlayCardClientPacket)},
+			{ Packet.AttachCard, typeof(AttachCardClientPacket)},
+			{ Packet.MoveCard, typeof(MoveCardClientPacket)},
+			{ Packet.DiscardCard, typeof(DiscardCardClientPacket)},
+			{ Packet.AnnihilateCard, typeof(AnnihilateCardClientPacket)},
+			//private areas
+			{ Packet.RehandCard, typeof(RehandCardClientPacket)},
+			{ Packet.TopdeckCard, typeof(TopdeckCardClientPacket)},
+			{ Packet.ReshuffleCard, typeof(ReshuffleCardClientPacket)},
+			{ Packet.BottomdeckCard, typeof(BottomdeckCardClientPacket)},
+			//stats
+			{ Packet.UpdateCardNumericStats, typeof(ChangeCardNumericStatsClientPacket)},
+			{ Packet.NegateCard, typeof(NegateCardClientPacket)},
+			{ Packet.ActivateCard, typeof(ActivateCardClientPacket)},
+			{ Packet.ChangeCardController, typeof(ChangeCardControllerClientPacket)},
+			{ Packet.SetPips, typeof(SetPipsClientPacket)},
+			{ Packet.AttacksThisTurn, typeof(AttacksThisTurnClientPacket)},
+			{ Packet.SpacesMoved, typeof(SpacesMovedClientPacket)},
+			//effects
+			//targeting
+			{ Packet.GetCardTarget, typeof(GetCardTargetClientPacket)},
+			{ Packet.GetSpaceTarget, typeof(GetSpaceTargetClientPacket)},
+			//other
+			{ Packet.GetEffectOption, typeof(GetEffectOptionClientPacket)},
+			{ Packet.EffectResolving, typeof(EffectResolvingClientPacket)},
+			{ Packet.EffectActivated, typeof(EffectActivatedClientPacket)},
+			{ Packet.RemoveStackEntry, typeof(RemoveStackEntryClientPacket)},
+			{ Packet.SetEffectsX, typeof(SetEffectsXClientPacket)},
+			{ Packet.PlayerChooseX, typeof(GetPlayerChooseXClientPacket)},
+			{ Packet.TargetAccepted, typeof(TargetAcceptedClientPacket)},
+			{ Packet.AddTarget, typeof(AddTargetClientPacket)},
+			{ Packet.RemoveTarget, typeof(RemoveTargetClientPacket)},
+			{ Packet.ToggleDecliningTarget, typeof(ToggleDecliningTargetClientPacket)},
+			{ Packet.StackEmpty, typeof(StackEmptyClientPacket)},
+			{ Packet.EffectImpossible, typeof(EffectImpossibleClientPacket)},
+			{ Packet.OptionalTrigger, typeof(OptionalTriggerClientPacket)},
+			{ Packet.ToggleAllowResponses, typeof(ToggleAllowResponsesClientPacket)},
+			{ Packet.GetTriggerOrder, typeof(GetTriggerOrderClientPacket)},
+			{ Packet.EditCardLink, typeof(EditCardLinkClientPacket)},
+		}; //TODO a unit test that asserts that all types in this map extend IClientOrderPacket
+
 		private IClientOrderPacket FromJson(string command, string json)
 		{
-			return command switch
-			{
-				//game start
-				Packet.GetDeck => JsonConvert.DeserializeObject<GetDeckClientPacket>(json),
-				Packet.DeckAccepted => JsonConvert.DeserializeObject<DeckAcceptedClientPacket>(json),
-				Packet.SetAvatar => JsonConvert.DeserializeObject<SetAvatarClientPacket>(json),
-				Packet.SetFirstTurnPlayer => JsonConvert.DeserializeObject<SetFirstPlayerClientPacket>(json),
-				//gamestate
-				Packet.SetLeyload => JsonConvert.DeserializeObject<SetLeyloadClientPacket>(json),
-				Packet.SetTurnPlayer => JsonConvert.DeserializeObject<SetTurnPlayerClientPacket>(json),
-				Packet.PutCardsBack => JsonConvert.DeserializeObject<PutCardsBackClientPacket>(json),
-				Packet.AttackStarted => JsonConvert.DeserializeObject<AttackStartedClientPacket>(json),
-				Packet.HandSizeToStack => JsonConvert.DeserializeObject<HandSizeToStackClientPacket>(json),
-				Packet.ChooseHandSize => JsonConvert.DeserializeObject<GetHandSizeChoicesClientPacket>(json),
-				Packet.SetDeckCount => JsonConvert.DeserializeObject<SetDeckCountClientPacket>(json),
-				//card addition/deletion
-				Packet.AddCard => JsonConvert.DeserializeObject<AddCardClientPacket>(json),
-				Packet.DeleteCard => JsonConvert.DeserializeObject<DeleteCardClientPacket>(json),
-				Packet.ChangeEnemyHandCount => JsonConvert.DeserializeObject<ChangeEnemyHandCountClientPacket>(json),
-				//card movement
-				Packet.KnownToEnemy => JsonConvert.DeserializeObject<UpdateKnownToEnemyClientPacket>(json),
-				//public areas
-				Packet.PlayCard => JsonConvert.DeserializeObject<PlayCardClientPacket>(json),
-				Packet.AttachCard => JsonConvert.DeserializeObject<AttachCardClientPacket>(json),
-				Packet.MoveCard => JsonConvert.DeserializeObject<MoveCardClientPacket>(json),
-				Packet.DiscardCard => JsonConvert.DeserializeObject<DiscardCardClientPacket>(json),
-				Packet.AnnihilateCard => JsonConvert.DeserializeObject<AnnihilateCardClientPacket>(json),
-				//private areas
-				Packet.RehandCard => JsonConvert.DeserializeObject<RehandCardClientPacket>(json),
-				Packet.TopdeckCard => JsonConvert.DeserializeObject<TopdeckCardClientPacket>(json),
-				Packet.ReshuffleCard => JsonConvert.DeserializeObject<ReshuffleCardClientPacket>(json),
-				Packet.BottomdeckCard => JsonConvert.DeserializeObject<BottomdeckCardClientPacket>(json),
-				//stats
-				Packet.UpdateCardNumericStats => JsonConvert.DeserializeObject<ChangeCardNumericStatsClientPacket>(json),
-				Packet.NegateCard => JsonConvert.DeserializeObject<NegateCardClientPacket>(json),
-				Packet.ActivateCard => JsonConvert.DeserializeObject<ActivateCardClientPacket>(json),
-				Packet.ChangeCardController => JsonConvert.DeserializeObject<ChangeCardControllerClientPacket>(json),
-				Packet.SetPips => JsonConvert.DeserializeObject<SetPipsClientPacket>(json),
-				Packet.AttacksThisTurn => JsonConvert.DeserializeObject<AttacksThisTurnClientPacket>(json),
-				Packet.SpacesMoved => JsonConvert.DeserializeObject<SpacesMovedClientPacket>(json),
-				//effects
-				//targeting
-				Packet.GetCardTarget => JsonConvert.DeserializeObject<GetCardTargetClientPacket>(json),
-				Packet.GetSpaceTarget => JsonConvert.DeserializeObject<GetSpaceTargetClientPacket>(json),
-				//other
-				Packet.GetEffectOption => JsonConvert.DeserializeObject<GetEffectOptionClientPacket>(json),
-				Packet.EffectResolving => JsonConvert.DeserializeObject<EffectResolvingClientPacket>(json),
-				Packet.EffectActivated => JsonConvert.DeserializeObject<EffectActivatedClientPacket>(json),
-				Packet.RemoveStackEntry => JsonConvert.DeserializeObject<RemoveStackEntryClientPacket>(json),
-				Packet.SetEffectsX => JsonConvert.DeserializeObject<SetEffectsXClientPacket>(json),
-				Packet.PlayerChooseX => JsonConvert.DeserializeObject<GetPlayerChooseXClientPacket>(json),
-				Packet.TargetAccepted => JsonConvert.DeserializeObject<TargetAcceptedClientPacket>(json),
-				Packet.AddTarget => JsonConvert.DeserializeObject<AddTargetClientPacket>(json),
-				Packet.RemoveTarget => JsonConvert.DeserializeObject<RemoveTargetClientPacket>(json),
-				Packet.ToggleDecliningTarget => JsonConvert.DeserializeObject<ToggleDecliningTargetClientPacket>(json),
-				Packet.StackEmpty => JsonConvert.DeserializeObject<StackEmptyClientPacket>(json),
-				Packet.EffectImpossible => JsonConvert.DeserializeObject<EffectImpossibleClientPacket>(json),
-				Packet.OptionalTrigger => JsonConvert.DeserializeObject<OptionalTriggerClientPacket>(json),
-				Packet.ToggleAllowResponses => JsonConvert.DeserializeObject<ToggleAllowResponsesClientPacket>(json),
-				Packet.GetTriggerOrder => JsonConvert.DeserializeObject<GetTriggerOrderClientPacket>(json),
-				Packet.EditCardLink => JsonConvert.DeserializeObject<EditCardLinkClientPacket>(json),
-				//misc
-				_ => throw new System.ArgumentException($"Unrecognized command {command} in packet sent to client"),
-			};
+			if (!jsonTypes.ContainsKey(command)) throw new System.ArgumentException($"Unrecognized command {command} in packet sent to client");
+
+			return JsonConvert.DeserializeObject(json, jsonTypes[command]) as IClientOrderPacket;
 		}
 
 		public override Task ProcessPacket((string command, string json) packetInfo)
